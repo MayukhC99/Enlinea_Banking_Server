787d57eeafd85f3da8c7c95a0acaced44a29bf70
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-03-21 20:19:46 +0530

    Initialized session

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const app= express();
+const express_sessions = require('express-session');
+const passport= require('./passport');
 const path= require('path');
+
+const app= express();
+
 
 app.use(express.static(path.join(__dirname,'public')));
 
