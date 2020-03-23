28b65312b94e5a44c8d5ba37d9ad0fa594509252
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-03-23 20:43:16 +0530

    unlinking previous uploaded file

--- route/root.js
+++ route/root.js
@@ -2,7 +2,8 @@ const express= require('express');
 const multer= require('multer');
 const path= require('path');
 const db= require('../database').db;
-const sequelize= require('sequelize')
+const users= require('../database').users;
+const fs= require('fs');
 const route= express.Router();
 
 //Set Storage Engine
@@ -10,7 +11,7 @@ const storage_engine = multer.diskStorage({
     destination: '../public/uploads/',
     filename: function(req,file,done){
 
-        done(null,req.user.username+path.extname(file.originalname));//path.extname can extract extension name from file name
+        done(null,req.user.username+'-'+Date.now()+path.extname(file.originalname));//path.extname can extract extension name from file name
     }
 });
 
@@ -46,15 +47,25 @@ route.post('/upload/profile_image',(req,res)=>{
             if(req.file === undefined){
                 res.send(undefined);
             } else {
-                const str= '.\\uploads\\'+req.file.filename;
+                
+                if(req.user.profile_picture !== '000.png'){
+                    //deleting the file
+                    fs.unlink('../public/uploads/'+req.user.profile_picture , (err) => {
+                        if (err) throw err;
+                        console.log('The file has been deleted');
+                    });
+                }
                 db.query(`UPDATE users SET profile_picture=${req.file.filename} WHERE username= ${req.user.username}`);
-                res.send(str);
+                res.send('.\\uploads\\'+req.file.filename);
             }
         }
     })
 })
 
-
+//get profile picture
+route.get('/get/profile_picture',(req,res)=>{
+  res.send(req.user.profile_picture);
+})
 
 route.get('/verify_user',(req,res)=>{
     console.log('Verifying User');
