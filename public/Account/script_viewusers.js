3ae17246e1a7458f336594f1f7ac8c96eaf3df84
Supratim Saha <supratim1703@gmail.com>
2020-05-20 00:49:35 +0530

    Updated viewusers

--- public/Account/script_viewusers.js
+++ public/Account/script_viewusers.js
@@ -4,6 +4,7 @@ $(function(){
     let name = $('#my_name');
     let image = $('#user');
     let url = window.location.href;
+    var theOpenButton = document.querySelector('#accountcontainer');
 
     let username= url.split('/');
     username= username[username.length - 1];
@@ -12,4 +13,18 @@ $(function(){
         name.html(`<h1>${data.first_name + ' ' + data.last_name}</h1>`);
         image.attr('src', `../uploads/${data.profile_picture}`);
     })
+
+    $(document).mouseup(function(e){
+        var container = $("#accountcontainer");
+        if(e.target.id === "tog"){
+            if (theOpenButton.style.display === "none") {
+                theOpenButton.style.display = "grid";
+            } else {
+                theOpenButton.style.display = "none";
+            }
+        }
+        else{
+            container.hide();
+        }
+    });
 })
