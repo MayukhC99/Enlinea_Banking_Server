9ce8c2c2f1736fb1648ae1c6b055d2ac4a9ae172
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-05-20 09:25:35 +0530

    deactivate or delete account api updated

--- route/admin.js
+++ route/admin.js
@@ -4,10 +4,75 @@ const db= require('../database').db;
 const users= require('../database').users;
 const route= express.Router();
 
-route.post('/deactive',(req,res)=>{
-    
+route.post('/deactivate',(req,res)=>{
+    account_status.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then((user)=>{
+
+        if(!user){
+            console.log("User doesn't exist");
+            res.redirect('back');
+        }
+
+        db.query(`UPDATE account_status SET status='deactive' WHERE username='${req.body.username}'`);
+        console.log('updated successfully');
+
+        res.redirect('back');
+
+    }).catch((err)=>{
+        console.log(err);
+        res.redirect('back');
+    })
 })
 
+route.post('/activate',(req,res)=>{
+    account_status.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then((user)=>{
+
+        if(!user){
+            console.log("User doesn't exist");
+            res.redirect('back');
+        }
+
+        db.query(`UPDATE account_status SET status='active' WHERE username='${req.body.username}'`);
+        console.log('updated successfully');
+
+        res.redirect('back');
+
+    }).catch((err)=>{
+        console.log(err);
+        res.redirect('back');
+    })
+})
+
+route.delete('/delete_user',(req,res)=>{
+    account_status.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then((user)=>{
+
+        if(!user){
+            console.log("User doesn't exist");
+            res.redirect('back');
+        }
+
+        db.query(`DELETE FROM users WHERE username='${req.body.username}'`);
+        db.query(`DELETE FROM account_status WHERE username='${req.body.username}'`);
+        console.log('deleted successfully');
+
+        res.redirect('back');
+
+    }).catch((err)=>{
+        console.log(err);
+        res.redirect('back');
+    })
+})
 
 module.exports= {
     route
