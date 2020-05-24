bcb65234c22ebac6597b25cde23bed14dabef941
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-05-24 18:01:44 +0530

    generating friend request status

--- route/friend_request.js
+++ route/friend_request.js
@@ -2,13 +2,35 @@ const express= require('express');
 const db= require('../database').db;
 const users= require('../database').users;
 const friends= require('../database').friends;
+const path= require('path');
 const route= express.Router();
 
 route.post('/status',(req,res)=>{
-    if(req.user){
-        
+
+    if(req.user && req.user.message != "deactivated"){
+
+        friends.findOne({
+            where: {
+                username: req.user.username,
+                requested_user: req.body.username
+            }
+        }).then((user)=>{
+            
+            if(!user){
+                console.log('requested user for friend request status, not found');
+                res.send(undefined); //if no friend request has been sent yet
+            }
+
+            console.log('requested user for friend request status, found');
+            res.send(user); // sending friend request status such as pending or accepted
+        })
+
+    } else {
+        res.sendFile(path.join(__dirname,'..','public','login','login.html'));
     }
 })
+
+
 
 
 module.exports={
